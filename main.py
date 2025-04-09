3a291f95cb4ca2c0688632d0e806bd0752227261
eugene-sakwa <the.ardent.arian@gmail.com>
2025-04-09 10:35:12 +0200

    Apply class and instance attributes to the object

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 class Item:
+
+    pay_rate = 0.8 # The pay rate after 20% discount
     def __init__(self, name: str, price: float, quantity = 0):
         # Run validation to the received arguments
         assert price >= 0, f'Price {price} is not greater than or equal to 0'
@@ -10,16 +12,13 @@ class Item:
         self.quantity = quantity
     def calculate_total_price(self):
         return self.price * self.quantity
+    
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
 
 
-
-item1 = Item('Phone', 100, 5)
-item1.name = 'Phone'
-item1.price = 100
-item1.quantity = 5
-
-
-item2 = Item('Laptop', 1000, 3)
-item2.name = 'Laptop'
-item2.price = 1000
-item2.quantity = 3
+item1 = Item('Phone', 100, 1)
+item2 = Item( 'Laptop', 1000, 3)
+item3 = Item('Cable', 10, 5)
+item4 = Item( 'Mouse', 50, 5)
+item5 = Item('Keyboard', 75, 5)

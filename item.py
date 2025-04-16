b940d5fac895c23cc1a8e4ccc925e5bf95371dd7
eugene-sakwa <the.ardent.arian@gmail.com>
2025-04-16 20:23:21 +0200

    Refactor: Add encapuslation for price attribue using getter and setter

--- item.py
+++ item.py
@@ -11,31 +11,47 @@ class Item:
         
         # Assign to self object
         self.__name = name
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
         # Actions to execute
         Item.all.append(self)
 
-    @property
     #Property Decorator = Read-Only Attribute
+    @property
     def name(self):
         return self.__name
     
-    @name.setter
+    @property
+    def price(self):
+        return self.__price
+    
+    def apply_discount(self):
+        self.__price = self.__price * self.pay_rate
+
+    def apply_increament(self, increament_value):
+        self.__price = self.__price + self.price * increament_value
+
+    
     #Property Decorator = Set attribute for read only 
+    @name.setter
     def name(self, new_name):
         if len(new_name) > 10:
             raise Exception( 'The name is too long!')
         else:
             self.__name = new_name
+
+    @price.setter
+    def price(self, new_price):
+        if new_price < 0:
+            raise Exception( 'The price cannot be below 0')
+        else:
+            self.__price = new_price
     
 
     def calculate_total_price(self):
-        return self.price * self.quantity
+        return self.__price * self.quantity
     
-    def apply_discount(self):
-        self.price = self.price * self.pay_rate
 
     @classmethod
     def instantiate_from_csv(cls):
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from item import Item
 item1 = Item('MyItem', 750)
 
 
-item1.name = 'OtherItssssssem'
+item1.apply_increament(0.2)
 
-print(item1.name)
+print(item1.price)
 # print(item1.read_only_name)
